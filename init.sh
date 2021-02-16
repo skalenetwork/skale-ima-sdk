83dd9f3ba7c833d6cfae64994e2703e003e1d66c
Sergiy Lavrynenko <sergiy@skalelabs.com>
2021-02-16 12:34:32 +0200

    Updated reference to new IMA versiom

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ cd ${_DEV_DIR} || exit
 if [ ! -d ./IMA ]; then
     git clone https://github.com/skalenetwork/IMA.git --recursive \
         && cd IMA \
-        && git checkout dc572cae6f34a9bab1f98d84cba32e5748ac8457 \
+        && git checkout 80148d993d6c598c5d117618fb1f29feb682928a \
         && cp ../../MessageProxyForMainnet.sol ./proxy/contracts/MessageProxyForMainnet.sol \
         && cp ../../LockAndDataForMainnet.sol ./proxy/contracts/LockAndDataForMainnet.sol \
         && yarn install \
