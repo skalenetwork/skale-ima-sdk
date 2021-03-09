df053183bda95321f33fde86249e189431d7797e
Artem Payvin <payvinartem@gmail.com>
2021-03-09 23:47:47 +0200

    Update checkout to the branch instead of commit

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ cd ${_DEV_DIR} || exit
 if [ ! -d ./IMA ]; then
     git clone https://github.com/skalenetwork/IMA.git --recursive \
         && cd IMA \
-        && git checkout a7e32a76024a09f8494f902b92d558b3f791f01c \
+        && git checkout enhancement/SKALE-3871-registry-for-message-proxy \
         && cp ../../MessageProxyForMainnet.sol ./proxy/contracts/MessageProxyForMainnet.sol \
         && cp ../../LockAndDataForMainnet.sol ./proxy/contracts/LockAndDataForMainnet.sol \
         && yarn install \
